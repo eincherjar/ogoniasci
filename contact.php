f9ab10d2eed273235dd7aa06fa6d37e89fa8af43
Marcin Barszcz <marcinbarszcz@int.pl>
2022-12-19 13:46:48 +0000

    nowy kontaky nie dziła

--- contact.php
+++ contact.php
@@ -1,79 +1,30 @@
 <?php
-// require ReCaptcha class
-require('recaptcha/src/autoload.php');
-// configure
-// an email address that will be in the From field of the email.
-$from = $_POST['email'];
-// $from = 'marcin@globetyre.pl';
-// an email address that will receive the email with the output of the form
-$sendTo = 'marcin@globetyre.pl';
-// subject of the email
-$subject = 'New message from Ogoniasci.pl';
-// form field names and their translations.
-// array variable name => Text to appear in the email
-$fields = array('name' => 'Name', 'email' => 'Email', 'message' => 'Message');
-// message that will be displayed when everything is OK :)
-$okMessage = '<div style="text-align:center;margin-top:25%;"><h1>Contact form successfully submitted. Thank you, I will get back to you soon!</h1><a href="/">Back to page</a></div>';
-// If something goes wrong, we will display this message.
-$errorMessage = '<div style="text-align:center;margin-top:25%;"><h1>There was an error while submitting the form. Please try again later</h1><a href="/">Back to page</a></div>';
-// ReCaptch Secret
-$recaptchaSecret = '6Lf4aI8jAAAAAKwm6FRCvdqWb8cNaPtIz2duppQE';
-// let's do the sending
-// if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
-error_reporting(E_ALL & ~E_NOTICE);
-try {
-    if (!empty($_POST)) {
-        // validate the ReCaptcha, if something is wrong, we throw an Exception,
-        // i.e. code stops executing and goes to catch() block
+if(isset($_POST['name']) && $_POST['name']!="" && isset($_POST['email']) && $_POST['email']!="")
+{
+   // This is Google API url where we pass the API secret key to validate the user request.
+   $google_recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
+   $recaptcha_secret_key = '6Lf4aI8jAAAAAKwm6FRCvdqWb8cNaPtIz2duppQE'; // Add your generated Secret key
+  $set_recaptcha_response = $_POST['recaptcha_response'];
+   // Make the request and capture the response by making below request.
+   $get_recaptcha_response = file_get_contents($google_recaptcha_url . '?secret=' . $recaptcha_secret_key . '&response=' . $set_recaptcha_response);
 
-        if (!isset($_POST['g-recaptcha-response'])) {
-            throw new \Exception('ReCaptcha is not set.');
-        }
-        // do not forget to enter your secret key from https://www.google.com/recaptcha/admin
-
-        $recaptcha = new \ReCaptcha\ReCaptcha($recaptchaSecret, new \ReCaptcha\RequestMethod\CurlPost());
-
-        // we validate the ReCaptcha field together with the user's IP address
-
-        $response = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
-        if (!$response->isSuccess()) {
-            /*
-            Uncomment this to see the error codes if you have troubles with the implementation.
-            I recommend not to display the error codes on production, though.
-            $errors = $response->getErrorCodes();
-            throw new \Exception('ReCaptcha returned error: ', implode(',', $errors));
-            */
-            throw new \Exception('ReCaptcha was not validated.');
-        }
-
-        // everything went well, we can compose the message, as usually
-
-        $emailText = "You have a new message:\n=============================\n";
-        foreach ($_POST as $key => $value) {
-            // If the field exists in the $fields array, include it in the email
-            if (isset($fields[$key])) {
-                $emailText .= "$fields[$key]: $value\n";
-            }
-        }
-
-        // All the neccessary headers for the email.
-        $headers = array('Content-Type: text/plain; charset="UTF-8";',
-            'From: ' . $from,
-            // 'Reply-To: ' . $from,
-            // 'Return-Path: ' . $from,
-        );
-
-        // Send email
-        mail($sendTo, $subject, $emailText, implode("\n", $headers));
-        $responseArray = array('type' => 'success', 'message' => $okMessage);
-    }
-} catch (\Exception $e) {
-    $responseArray = array('type' => 'danger', 'message' => $e->getMessage());
-}
-if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-    $encoded = json_encode($responseArray);
-    header('Content-Type: application/json');
-    echo $encoded;
+   print( $get_recaptcha_response);
+   $get_recaptcha_response = json_decode($get_recaptcha_response);
+   $success_msg="";
+   $err_msg="";
+   // Set the Google recaptcha spam score here and based the score, take your action
+   if ($get_recaptcha_response->success == true && $get_recaptcha_response->score >= 0.5 && $get_recaptcha_response->action == 'submit') {
+       $success_msg = "Your message has been sent successfully.";
+   } else {
+       $err_msg = "Something went wrong. Please try again after sometime.";
+   }
 } else {
-    echo $responseArray['message'];
+   $err_msg = "Please enter the required fields in this form";
 }
+// Get the response and pass it into your ajax as a response.
+$return_msg = array(
+   'error'     =>  $err_msg,
+   'success'   =>  $success_msg
+);
+echo json_encode($return_msg);
+?>
