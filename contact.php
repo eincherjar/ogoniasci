bb2cf22435540176155f4b7d261be101454678ab
Marcin Barszcz <marcinbarszcz@int.pl>
2022-12-19 14:34:03 +0000

    nowy formularz kontaktu

--- contact.php
+++ contact.php
@@ -1,30 +1,86 @@
 <?php
-if(isset($_POST['name']) && $_POST['name']!="" && isset($_POST['email']) && $_POST['email']!="")
-{
-   // This is Google API url where we pass the API secret key to validate the user request.
-   $google_recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
-   $recaptcha_secret_key = '6Lf4aI8jAAAAAKwm6FRCvdqWb8cNaPtIz2duppQE'; // Add your generated Secret key
-  $set_recaptcha_response = $_POST['recaptcha_response'];
-   // Make the request and capture the response by making below request.
-   $get_recaptcha_response = file_get_contents($google_recaptcha_url . '?secret=' . $recaptcha_secret_key . '&response=' . $set_recaptcha_response);
 
-   print( $get_recaptcha_response);
-   $get_recaptcha_response = json_decode($get_recaptcha_response);
-   $success_msg="";
-   $err_msg="";
-   // Set the Google recaptcha spam score here and based the score, take your action
-   if ($get_recaptcha_response->success == true && $get_recaptcha_response->score >= 0.5 && $get_recaptcha_response->action == 'submit') {
-       $success_msg = "Your message has been sent successfully.";
-   } else {
-       $err_msg = "Something went wrong. Please try again after sometime.";
-   }
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+  # BEGIN Setting reCaptcha v3 validation data
+  $url = "https://www.google.com/recaptcha/api/siteverify";
+  $data = [
+    'secret' => "6Lf4aI8jAAAAAKwm6FRCvdqWb8cNaPtIz2duppQE",
+    'response' => $_POST['token'],
+    'remoteip' => $_SERVER['REMOTE_ADDR']
+  ];
+
+  $options = array(
+    'http' => array(
+      'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+      'method'  => 'POST',
+      'content' => http_build_query($data)
+    )
+    );
+
+  # Creates and returns stream context with options supplied in options preset
+  $context  = stream_context_create($options);
+  # file_get_contents() is the preferred way to read the contents of a file into a string
+  $response = file_get_contents($url, false, $context);
+  # Takes a JSON encoded string and converts it into a PHP variable
+  $res = json_decode($response, true);
+  # END setting reCaptcha v3 validation data
+
+    // print_r($response);
+# Post form OR output alert and bypass post if false. NOTE: score conditional is optional
+# since the successful score default is set at >= 0.5 by Google. Some developers want to
+# be able to control score result conditions, so I included that in this example.
+
+  if ($res['success'] == true && $res['score'] >= 0.5) {
+
+    # Recipient email
+    $mail_to = "marcin@globetyre.pl";
+
+    # Sender form data
+    $subject = trim($_POST["subject"]);
+    $name = str_replace(array("\r","\n"),array(" "," ") , strip_tags(trim($_POST["name"])));
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $phone = trim($_POST["phone"]);
+    $message = trim($_POST["message"]);
+
+    if (empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR empty($phone) OR empty($subject) OR empty($message)) {
+      # Set a 400 (bad request) response code and exit
+      http_response_code(400);
+      echo '<p class="alert-warning">Please complete the form and try again.</p>';
+      exit;
+    }
+
+    # Mail content
+    $content = "Name: $name\n";
+    $content .= "Email: $email\n\n";
+    $content .= "Phone: $phone\n";
+    $content .= "Message:\n$message\n";
+
+    # Email headers
+    $headers = "From: $name <$email>";
+
+    # Send the email
+    $success = mail($mail_to, $subject, $content, $headers);
+
+    if ($success) {
+      # Set a 200 (okay) response code
+      http_response_code(200);
+      echo '<p class="alert alert-success">Thank You! Your message has been successfully sent.</p>';
+    } else {
+      # Set a 500 (internal server error) response code
+      http_response_code(500);
+      echo '<p class="alert alert-warning">Something went wrong, your message could not be sent.</p>';
+    }
+
+  } else {
+
+    echo '<div class="alert alert-danger">
+        Error! The security token has expired or you are a bot.
+       </div>';
+  }
+
 } else {
-   $err_msg = "Please enter the required fields in this form";
-}
-// Get the response and pass it into your ajax as a response.
-$return_msg = array(
-   'error'     =>  $err_msg,
-   'success'   =>  $success_msg
-);
-echo json_encode($return_msg);
-?>
+  # Not a POST request, set a 403 (forbidden) response code
+  http_response_code(403);
+  echo '<p class="alert-warning">There was a problem with your submission, please try again.</p>';
+} ?>
